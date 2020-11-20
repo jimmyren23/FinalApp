837a6da9300e26313a5bd4eb452fd41a3a6fe2ad
Jimmy Ren <laurinaluo@Jimmys-MacBook-Pro.local>
2020-11-20 12:23:58 -0600

    initial commit

---
+++ FinalApp/FinalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalApp
+//
+//  Created by Jimmy Ren on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
